1c28184d657a8bd4ce768d2d2dca8f9ba42c1a2a
James Murphy <jamesivdeveloper@gmail.com>
2022-01-31 09:13:27 -0700

    generate spaces for letters of random word generated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var wordEl = document.querySelector("#word");
 var definitionEl = document.querySelector("#definition");
+var playArea = document.querySelector("#playarea");
 
-// fetch random word
+// fetch random word and definition
 var getRandomWord = function () {
   var apiUrl = "https://random-words-api.vercel.app/word";
 
@@ -20,8 +21,27 @@ var getRandomWord = function () {
 // display word and definition
 var displayWord = function (data) {
   var word = data[0].word;
+  var definition = data[0].definition;
 
   wordEl.textContent = word;
+  definitionEl.textContent = "Definition: " + definition;
+
+  wordSpaces(word);
+};
+
+// display blank spaces for word
+var wordSpaces = function (word) {
+  console.log(word.length);
+  var wordEl = document.createElement("ul");
+
+  for (var i = 0; i < word.length; i++) {
+    var letterEl = document.createElement("li");
+    letterEl.id = "correct-letter";
+    letterEl.textContent = "_";
+
+    wordEl.appendChild(letterEl);
+    playArea.appendChild(wordEl);
+  }
 };
 
 // fetch happy gif
